c7faeff1adeb427f8950c9050b8124fb51c6e848
website-krysta <website.krysta@gmail.com>
2023-03-30 06:59:29 +0530

    fixed issues

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className={darkMode ? "app dark" : "app"}> 
-      <BrowserRouter>
+      <BrowserRouter basename='krystafrontend'>
         <Routes>
           <Route path="/">
             <Route index element={<Login />} />
